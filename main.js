e384799e90a2cd99dae72f2ba3c6dcd089e7af3b
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2025-04-25 12:26:19 +0200

    menu

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+// Mobile Menu
+// A $( document ).ready() block.
+$(document).ready(function () {
+    $(document).ready(function () {
+        $(".mobile-button").click(function () {
+            $(".mobile-button").toggleClass("active");
+            $(".mobile-menu").toggleClass("active");
+        });
+    });
+});
+
+// Slick
+$('.testimonial-container').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500,
+    arrows: false,
+    accessibility: true,
+    dots: true,
+    fade: true,
+    infinite: true,
+    pauseOnHover: true,
+});
+
+
 // Animate on Scroll
 AOS.init ({
 });
